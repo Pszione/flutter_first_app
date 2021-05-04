f056d23469ea9f914cfeffa3f1f2243cbd2d95e2
Pszione <oused.games@gmail.com>
2021-05-03 22:02:59 -0300

    google_fonts package | learning Colors.amber, // 0xFFFFC107

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,17 +17,20 @@ class MyApp extends StatelessWidget {
         // changing the primarySwatch below to Colors.green and then invoke "hot reload"
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.amber,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
         textTheme: TextTheme(
           headline1: TextStyle(),
-          headline4: TextStyle(color: Colors.redAccent),
+          headline4: GoogleFonts.lobster(color: Colors.redAccent, letterSpacing: 2,),
           bodyText2: TextStyle(fontSize: 18),
-          subtitle1: TextStyle(fontStyle: FontStyle.italic),
+          subtitle1: TextStyle(fontStyle: FontStyle.italic, color: Color(0xFF00FF00),),
         ),
         accentIconTheme: IconThemeData(
-          size: 33
+          size: 33,
         ),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        //primarySwatch: Colors.amber, // 0xFFFFC107
+        //primarySwatch: MaterialColor(0xFFFFC107, {50: Color(), 100: Color()},),
+        primaryColor: Color(0xFFFFC107), // let Material create all other variants
+        accentColor: Color(0xFFFFFFFF), // 0xFF + copy HEX then (688e26)
       ),
       home: MyHomePage(title: 'Flutter First App'),
     );
@@ -92,6 +96,11 @@ class _MyHomePageState extends State<MyHomePage> {
             Text(
               'Like tapped $_counter times!',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            SizedBox(height: 25,),
+            Text(
+              'another random text rs',
+              style: Theme.of(context).textTheme.subtitle1,
             ),
             // Text(
             //   'You have pushed the button this many times:',
