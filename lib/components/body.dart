cb3b639ed0cf827a05ede9b2965be0bff97f370c
Pszione <oused.games@gmail.com>
2021-06-02 10:47:45 -0300

    Shop ecommerce - Ep 1

--- lib/components/body.dart
+++ lib/components/body.dart
@@ -13,29 +13,47 @@ class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     final SizeConfig _sizes = SizeConfig().init(context);
-    return Column(
-      children: <Widget>[
-        Expanded(
-          flex: 3,
-          // child: Container(
-          //   color: Colors.black,
-          // ),
-          child: SafeArea(
-            child: Text(
-              'ECOMMERCE',
-              style: TextStyle(
-                fontSize: _sizes.getProportionateScreenWidth(36),
-                color: kPrimaryColor,
-                fontWeight: FontWeight.bold,
+    return SafeArea(
+      child: SizedBox(
+        // What double.infinity means?
+        width: double.infinity,
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              flex: 3,
+              // child: Container(
+              //   color: Colors.black,
+              // ),
+              child: Column(
+                children: <Widget>[
+                  Spacer(),
+                  Text(
+                    'ECOMMERCE',
+                    style: TextStyle(
+                      fontSize: _sizes.getProportionateScreenWidth(36),
+                      color: kPrimaryColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text("Welcome to your Ecommerce, let's shop!"),
+                  Spacer(
+                    flex: 2,
+                  ),
+                  Image.asset(
+                    'assets/images/splash_1.png',
+                    width: _sizes.getProportionateScreenWidth(235),
+                    height: _sizes.getProportionateScreenHeight(265),
+                  ),
+                ],
               ),
             ),
-          ),
+            Expanded(
+              flex: 2,
+              child: SizedBox(),
+            ),
+          ],
         ),
-        Expanded(
-          flex: 2,
-          child: SizedBox(),
-        ),
-      ],
+      ),
     );
   }
 }
