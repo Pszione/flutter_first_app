c705f609d56de9ad6d58d7bb1a47b1c76796779d
Pszione <oused.games@gmail.com>
2021-06-06 00:46:04 -0300

    Shop ecommerce - Ep 2 - Sign In screen incomplete

--- lib/shop_ecommerce/sign_in/body.dart
+++ lib/shop_ecommerce/sign_in/body.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_first_app/constants_shop.dart';
+import 'package:flutter_first_app/size_config.dart';
+
 class Body extends StatelessWidget {
   const Body({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final SizeConfig _sizes = SizeConfig().init(context);
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: _sizes.getProportionateScreenWidth(20),
+          ),
+          child: Column(
+            children: [
+              Text(
+                'Welcome Back',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: _sizes.getProportionateScreenWidth(28),
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Sign in with your email and password \nor continue with social media',
+                textAlign: TextAlign.center,
+              ),
+              SignForms()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SignForms extends StatefulWidget {
+  const SignForms({Key key}) : super(key: key);
+
+  @override
+  _SignFormsState createState() => _SignFormsState();
+}
+
+class _SignFormsState extends State<SignForms> {
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'Enter our email',
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/shop_ecommerce/sign_in/shop_sign_in_screen.dart
+++ lib/shop_ecommerce/sign_in/shop_sign_in_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/shop_ecommerce/sign_in/body.dart';
+
+import '../../size_config.dart';
 
 class ShopSignInScreen extends StatelessWidget {
   const ShopSignInScreen({Key key}) : super(key: key);
   static String routeName = '/sign_in';
   @override
   Widget build(BuildContext context) {
+    // Call init on your start screen
+    SizeConfig().init(context);
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text('Sign In')),
       ),
+      body: Body(),
     );
   }
 }
