4892df0b9044ce96e07719c00bf3aa88d3e471ec
Pszione <oused.games@gmail.com>
2021-06-10 23:47:53 -0300

    Shop ecommerce - Ep 2 - Forgot Password screen part II

--- lib/shop_ecommerce/forgot_password/components/body.dart
+++ lib/shop_ecommerce/forgot_password/components/body.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/components/custom_suffix_icon.dart';
+import 'package:flutter_first_app/components/form_error.dart';
+import 'package:flutter_first_app/shop_ecommerce/components/default_big_button.dart';
 
 import '../../../size_config.dart';
 
@@ -8,22 +11,80 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final SizeConfig _sizes = SizeConfig().init(context);
-    return SizedBox(
-      width: double.infinity,
+    return SafeArea(
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: _sizes.getProportionateScreenWidth(20),
+          ),
+          child: Column(
+            children: <Widget>[
+              SizedBox(height: SizeConfig.screenHeight * 0.04),
+              Text(
+                'Forgot Password',
+                style: TextStyle(
+                  fontSize: _sizes.getProportionateScreenWidth(28),
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Please enter your email and we will send \nyou a link to return to your account',
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+              ForgotPasswordForm(),
+              SizedBox(height: SizeConfig.screenHeight * 0.1),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ForgotPasswordForm extends StatefulWidget {
+  const ForgotPasswordForm({Key key}) : super(key: key);
+
+  @override
+  _ForgotPasswordFormState createState() => _ForgotPasswordFormState();
+}
+
+class _ForgotPasswordFormState extends State<ForgotPasswordForm> {
+  String email;
+  final List<String> errors = <String>['Any error'];
+  @override
+  Widget build(BuildContext context) {
+    final SizeConfig _sizes = SizeConfig().init(context);
+    return Form(
       child: Column(
         children: <Widget>[
-          Text(
-            'Forgot your Password',
-            style: TextStyle(
-              fontSize: _sizes.getProportionateScreenWidth(28),
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            decoration: InputDecoration(
+              labelText: 'Email',
+              hintText: 'Enter your recovery email',
+              //floatingLabelBehavior: FloatingLabelBehavior.always,
+              suffixIcon: CustomSuffixIcon(
+                sizes: _sizes,
+                svgIcon: 'assets/icons/Mail.svg',
+              ),
             ),
           ),
-          Text(
-            'Please enter your email and we will send \nyou a link to return to your account',
-            textAlign: TextAlign.center,
+          SizedBox(height: _sizes.getProportionateScreenHeight(30)),
+          FormErrorChecker(
+            sizes: _sizes,
+            errors: errors,
           ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          DefaultBigButton(
+            sizes: _sizes,
+            text: 'Continue',
+            onPress: () {},
+          ),
+          SizedBox(height: SizeConfig.screenHeight * 0.1),
+          // NoAccountText(),
         ],
       ),
     );
