8ab5f0d41eac1ff6c78428ec1159832904a79a5f
Pszione <oused.games@gmail.com>
2021-06-17 17:08:35 -0300

    Shop ecommerce - Fix sign_in forms

--- lib/shop_ecommerce/sign_in/body.dart
+++ lib/shop_ecommerce/sign_in/body.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_first_app/components/custom_suffix_icon.dart';
-import 'package:flutter_first_app/components/form_error.dart';
-
-import 'package:flutter_first_app/constants_shop.dart';
 import 'package:flutter_first_app/shop_ecommerce/components/default_big_button.dart';
-import 'package:flutter_first_app/shop_ecommerce/forgot_password/forgot_password_screen.dart';
+
+import 'package:flutter_first_app/shop_ecommerce/components/sign_forms.dart';
 import 'package:flutter_first_app/shop_ecommerce/login_success/shop_login_success_screen.dart';
 import 'package:flutter_first_app/size_config.dart';
 
@@ -41,171 +38,12 @@ class Body extends StatelessWidget {
                 ),
                 SizedBox(height: SizeConfig.screenHeight * 0.1),
                 SignForms(),
+                SizedBox(height: _sizes.getProportionateScreenHeight(50)),
+                NoAccountSignUp(sizes: _sizes),
                 SizedBox(height: SizeConfig.screenHeight * 0.1),
               ],
             ),
           ),
-        ),
-      ),
-    );
-  }
-}
-
-class SignForms extends StatefulWidget {
-  const SignForms({Key key}) : super(key: key);
-
-  @override
-  _SignFormsState createState() => _SignFormsState();
-}
-
-class _SignFormsState extends State<SignForms> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final List<String> errors = <String>[];
-  String email;
-  String password;
-  bool rememberMe = false;
-  @override
-  Widget build(BuildContext context) {
-    final SizeConfig _sizes = SizeConfig().init(context);
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[
-          buildEmailFormField(_sizes),
-          SizedBox(height: _sizes.getProportionateScreenHeight(20)),
-          buildPasswordFormField(_sizes),
-          SizedBox(height: _sizes.getProportionateScreenHeight(10)),
-          Row(
-            children: <Widget>[
-              Checkbox(
-                value: rememberMe,
-                activeColor: kPrimaryColor,
-                onChanged: (bool value) {
-                  setState(() {
-                    rememberMe = value;
-                  });
-                },
-              ),
-              Text('Remember me'),
-              Spacer(),
-              GestureDetector(
-                onTap: () => Navigator.pushNamed(
-                    context, ShopForgotPasswordScreen.routeName),
-                child: Text(
-                  'Forgot Password?',
-                  style: TextStyle(decoration: TextDecoration.underline),
-                ),
-              ),
-            ],
-          ),
-          FormErrorChecker(sizes: _sizes, errors: errors),
-          SizedBox(height: SizeConfig.screenHeight * 0.1),
-          DefaultBigButton(
-            sizes: _sizes,
-            text: 'Sign in!',
-            onPress: () {
-              if (_formKey.currentState.validate()) {
-                _formKey.currentState.save();
-                // Do want you want to do
-                Navigator.pushNamed(context, ShopLoginSuccessScreen.routeName);
-              }
-            },
-          ),
-          SizedBox(height: _sizes.getProportionateScreenHeight(50)),
-          NoAccountSignUp(sizes: _sizes),
-          SizedBox(height: SizeConfig.screenHeight * 0.1),
-        ],
-      ),
-    );
-  }
-
-  TextFormField buildEmailFormField(SizeConfig _sizes) {
-    return TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      onSaved: (String newValue) => email = newValue,
-      onChanged: (String fieldValue) {
-        if ((fieldValue.isNotEmpty) && errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.remove(kEmailNullError);
-          });
-        } else if (kEmailValidatorRegex.hasMatch(fieldValue) &&
-            errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.remove(kInvalidEmailError);
-          });
-        }
-        _formKey.currentState.validate();
-        return null;
-      },
-      //
-      validator: (String fieldValue) {
-        if ((fieldValue.isEmpty || fieldValue.length <= 3) &&
-            !errors.contains(kEmailNullError)) {
-          setState(() {
-            errors.add(kEmailNullError);
-          });
-          //return '';
-        } else if (!kEmailValidatorRegex.hasMatch(fieldValue) &&
-            !errors.contains(kInvalidEmailError)) {
-          setState(() {
-            errors.add(kInvalidEmailError);
-          });
-          //return '';
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: 'Email',
-        hintText: 'Enter your email',
-        //floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          sizes: _sizes,
-          svgIcon: 'assets/icons/Mail.svg',
-        ),
-      ),
-    );
-  }
-
-  TextFormField buildPasswordFormField(SizeConfig _sizes) {
-    return TextFormField(
-      obscureText: true,
-      onSaved: (String newValue) => password = newValue,
-      onChanged: (String fieldValue) {
-        if ((fieldValue.isNotEmpty) && errors.contains(kPassNullError)) {
-          setState(() {
-            errors.remove(kPassNullError);
-          });
-        } else if (fieldValue.length >= 6 && errors.contains(kShortPassError)) {
-          setState(() {
-            errors.remove(kShortPassError);
-          });
-        }
-        _formKey.currentState.validate();
-        return null;
-      },
-      //
-      validator: (String fieldValue) {
-        if ((fieldValue.isEmpty || fieldValue.length <= 3) &&
-            !errors.contains(kPassNullError)) {
-          setState(() {
-            errors.add(kPassNullError);
-          });
-          //return '';
-        } else if (fieldValue.length < 6 && !errors.contains(kShortPassError)) {
-          setState(() {
-            errors.add(kShortPassError);
-          });
-          //return '';
-        }
-        return null;
-      },
-      decoration: InputDecoration(
-        labelText: 'Password',
-        hintText: 'Enter your password',
-        //floatingLabelBehavior: FloatingLabelBehavior.always,
-        suffixIcon: CustomSuffixIcon(
-          sizes: _sizes,
-          svgIcon: 'assets/icons/Lock.svg',
         ),
       ),
     );
--- lib/shop_ecommerce/sign_up/body.dart
+++ lib/shop_ecommerce/sign_up/body.dart
@@ -17,7 +17,37 @@ class Body extends StatelessWidget {
             'Register Account',
             style: tHeadingStyle,
           ),
+          Text(
+            'Complete your details or continue \nwith social media',
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: _sizes.getProportionateScreenWidth(20)),
+          SignUpForms(),
         ],
+      ),
+    );
+  }
+}
+
+class SignUpForms extends StatefulWidget {
+  const SignUpForms({Key key}) : super(key: key);
+
+  @override
+  _SignUpFormsState createState() => _SignUpFormsState();
+}
+
+class _SignUpFormsState extends State<SignUpForms> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final List<String> errors = <String>[];
+  String email;
+  String password;
+  String passwordConfirmation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      child: Column(
+        children: <Widget>[],
       ),
     );
   }
