ddc6fb87c1559e87d3e29446e17aee6cb06bca83
Pszione <oused.games@gmail.com>
2021-06-28 20:04:18 -0300

    Shop ecommerce - Ep 3 - OTP SMS Verification screen part II

--- lib/shop_ecommerce/otp_sms/body.dart
+++ lib/shop_ecommerce/otp_sms/body.dart
@@ -27,10 +27,68 @@ class Body extends StatelessWidget {
                 'We sent your code to +55 898 860 ***',
                 textAlign: TextAlign.center,
               ),
+              buildTimerText(),
+              SizedBox(height: SizeConfig.screenHeight * 0.08),
+              OtpForm(),
               SizedBox(height: SizeConfig.screenHeight * 0.1),
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Row buildTimerText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text('This code will expire in '),
+        TweenAnimationBuilder<double>(
+          tween: Tween<double>(begin: 30.0, end: 0.0),
+          duration: const Duration(seconds: 30), // because we allow user 30s
+          builder: (BuildContext context, double value, Widget child) => Text(
+              '00:${value.toInt()}',
+              style: TextStyle(color: kPrimaryColor)),
+        ),
+      ],
+    );
+  }
+}
+
+class OtpForm extends StatefulWidget {
+  const OtpForm({Key key}) : super(key: key);
+
+  @override
+  _OtpFormState createState() => _OtpFormState();
+}
+
+class _OtpFormState extends State<OtpForm> {
+  @override
+  Widget build(BuildContext context) {
+    final SizeConfig _sizes = SizeConfig().init(context);
+    return Form(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          buildOtpInputNumber(_sizes),
+          buildOtpInputNumber(_sizes),
+          buildOtpInputNumber(_sizes),
+          buildOtpInputNumber(_sizes),
+        ],
+      ),
+    );
+  }
+
+  SizedBox buildOtpInputNumber(SizeConfig _sizes) {
+    return SizedBox(
+      width: _sizes.getProportionateScreenWidth(60),
+      child: TextFormField(
+        keyboardType: TextInputType.number,
+        // obscureText: true,
+        style: TextStyle(fontSize: 30),
+        textAlign: TextAlign.center,
+        decoration: tOtpInputDecoration,
+        onChanged: (String value) {},
       ),
     );
   }
