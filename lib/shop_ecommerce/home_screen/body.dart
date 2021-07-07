a66a6efe987d32a24cc991e1c26fb78b27e20f68
Pszione <oused.games@gmail.com>
2021-07-06 21:08:30 -0300

    Shop ecommerce - Ep 4 - SearchBar part II

--- lib/shop_ecommerce/home_screen/body.dart
+++ lib/shop_ecommerce/home_screen/body.dart
@@ -56,17 +56,46 @@ class Body extends StatelessWidget {
             ),
           ),
         ),
-        Container(
-          width: _sizes.getProportionateScreenWidth(46),
-          height: _sizes.getProportionateScreenWidth(46),
-          padding: EdgeInsets.all(_sizes.getProportionateScreenWidth(11)),
-          decoration: BoxDecoration(
-            color: kSecondaryColor.withOpacity(0.1),
-            shape: BoxShape.circle,
-          ),
-          child: SvgPicture.asset(
-            'assets/icons/Bell.svg',
-          ),
+        Stack(
+          // Sobrepor two widgets
+          clipBehavior: Clip.none, // Overflow.visible
+          children: <Widget>[
+            Container(
+              width: _sizes.getProportionateScreenWidth(46),
+              height: _sizes.getProportionateScreenWidth(46),
+              padding: EdgeInsets.all(_sizes.getProportionateScreenWidth(11)),
+              decoration: BoxDecoration(
+                color: kSecondaryColor.withOpacity(0.1),
+                shape: BoxShape.circle,
+              ),
+              child: SvgPicture.asset(
+                'assets/icons/Bell.svg',
+              ),
+            ),
+            Positioned(
+              right: 0, // positioned made it work
+              child: Container(
+                width: _sizes.getProportionateScreenWidth(16),
+                height: _sizes.getProportionateScreenWidth(16),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFF4848),
+                  shape: BoxShape.circle,
+                  border: Border.all(width: 1.5, color: Colors.white),
+                ),
+                child: Center(
+                  child: Text(
+                    '3',
+                    style: TextStyle(
+                      fontSize: _sizes.getProportionateScreenWidth(10),
+                      height: 1,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+              ),
+            )
+          ],
         ),
       ],
     );
