57f44dcb2831e6d3871011a1694c7b3854fa27b2
Pszione <oused.games@gmail.com>
2021-07-09 16:46:36 -0300

    Shop ecommerce - Ep 5 - Images preview carousel - Product Details part II *_*

--- lib/shop_ecommerce/product_details/body.dart
+++ lib/shop_ecommerce/product_details/body.dart
@@ -40,7 +40,7 @@ class _ProductImagesState extends State<ProductImages> {
           width: widget._sizes.getProportionateScreenWidth(238),
           child: AspectRatio(
             aspectRatio: 1,
-            child: Image.asset(widget.product.images[0]),
+            child: Image.asset(widget.product.images[selectedImage]),
           ),
         ),
         Row(
@@ -56,22 +56,29 @@ class _ProductImagesState extends State<ProductImages> {
     );
   }
 
-  Container buildSmallPreview(int index) {
-    return Container(
-      margin:
-          EdgeInsets.only(right: widget._sizes.getProportionateScreenWidth(15)),
-      padding: EdgeInsets.all(widget._sizes.getProportionateScreenWidth(4)),
-      width: widget._sizes.getProportionateScreenWidth(54),
-      height: widget._sizes.getProportionateScreenWidth(54),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(16),
-        border: Border.all(
-            color:
-                selectedImage == index ? kPrimaryColor : Colors.grey.shade300,
-            width: 2.2),
-      ), // 10
-      child: Image.asset(widget.product.images[index]),
+  GestureDetector buildSmallPreview(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedImage = index;
+        });
+      },
+      child: Container(
+        margin: EdgeInsets.only(
+            right: widget._sizes.getProportionateScreenWidth(15)),
+        padding: EdgeInsets.all(widget._sizes.getProportionateScreenWidth(4)),
+        width: widget._sizes.getProportionateScreenWidth(54),
+        height: widget._sizes.getProportionateScreenWidth(54),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+              color:
+                  selectedImage == index ? kPrimaryColor : Colors.grey.shade300,
+              width: 2.2),
+        ), // 10
+        child: Image.asset(widget.product.images[index]),
+      ),
     );
   }
 }
