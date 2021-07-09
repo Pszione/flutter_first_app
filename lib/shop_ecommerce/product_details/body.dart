325e1bb65d95052ada970fe2021abd9a8d62f509
Pszione <oused.games@gmail.com>
2021-07-09 16:42:31 -0300

    Shop ecommerce - Ep 5 - Product Details part II - ProductImagesPreview

--- lib/shop_ecommerce/product_details/body.dart
+++ lib/shop_ecommerce/product_details/body.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_first_app/shop_ecommerce/models/product.dart';
+import 'package:flutter_first_app/size_config.dart';
+
+import '../constants_shop.dart';
 
 class Body extends StatelessWidget {
   const Body({Key key, @required this.product}) : super(key: key);
@@ -7,10 +10,68 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SizedBox(
-        width: double.infinity,
-      ),
+    final SizeConfig _sizes = SizeConfig().init(context);
+    return ProductImages(sizes: _sizes, product: product);
+  }
+}
+
+class ProductImages extends StatefulWidget {
+  const ProductImages({
+    Key key,
+    @required SizeConfig sizes,
+    @required this.product,
+  })  : _sizes = sizes,
+        super(key: key);
+
+  final SizeConfig _sizes;
+  final Product product;
+
+  @override
+  State<ProductImages> createState() => _ProductImagesState();
+}
+
+class _ProductImagesState extends State<ProductImages> {
+  int selectedImage = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        SizedBox(
+          width: widget._sizes.getProportionateScreenWidth(238),
+          child: AspectRatio(
+            aspectRatio: 1,
+            child: Image.asset(widget.product.images[0]),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ...List.generate(
+              widget.product.images.length,
+              (int index) => buildSmallPreview(index),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Container buildSmallPreview(int index) {
+    return Container(
+      margin:
+          EdgeInsets.only(right: widget._sizes.getProportionateScreenWidth(15)),
+      padding: EdgeInsets.all(widget._sizes.getProportionateScreenWidth(4)),
+      width: widget._sizes.getProportionateScreenWidth(54),
+      height: widget._sizes.getProportionateScreenWidth(54),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16),
+        border: Border.all(
+            color:
+                selectedImage == index ? kPrimaryColor : Colors.grey.shade300,
+            width: 2.2),
+      ), // 10
+      child: Image.asset(widget.product.images[index]),
     );
   }
 }
