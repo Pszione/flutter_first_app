0309b258c291662541d34e18c93a13a43f11ce5b
Pszione <oused.games@gmail.com>
2021-07-13 13:39:56 -0300

    Shop ecommerce - Ep 7 - FINISH

--- lib/shop_ecommerce/home_screen/shop_home_screen.dart
+++ lib/shop_ecommerce/home_screen/shop_home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_first_app/shop_ecommerce/components/bottom_nav_bar_app.dart';
 import 'package:flutter_first_app/shop_ecommerce/home_screen/body.dart';
+
+import '../enums.dart';
 
 class ShopHomeScreen extends StatelessWidget {
   const ShopHomeScreen({Key key}) : super(key: key);
@@ -12,6 +15,7 @@ class ShopHomeScreen extends StatelessWidget {
       //   title: Text('Home Screen'),
       // ),
       body: Body(),
+      bottomNavigationBar: BottomNavBarApp(selectedMenu: MenuState.home),
     );
   }
 }
