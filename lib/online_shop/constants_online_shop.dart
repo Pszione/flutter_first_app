1b99cef8511539a680e96767236f7751ad8ced62
Pszione <oused.games@gmail.com>
2021-07-16 00:32:27 -0300

    Online Shop - Fix file names and more

--- lib/online_shop/constants_online_shop.dart
+++ lib/online_shop/constants_online_shop.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
-const kTextColor = Color(0xFF535353);
-const kTextLightColor = Color(0xFFACACAC);
+import '../size_config.dart';
 
-const kDefaultPaddin = 20.0;
+const Color kPrimaryColor = Color(0xFFFF7643);
+const Color kSecondaryColor = Color(0xFF979797);
+const Color kTextColor = Color(0xFF535353);
+const Color kTextLightColor = Color(0xFFACACAC);
+
+final EdgeInsetsGeometry kAppSafeBorder = EdgeInsets.symmetric(
+  horizontal: SizeConfig().getProportionateScreenWidth(20),
+);
+final double kAppSafeBorderAs = SizeConfig().getProportionateScreenWidth(20);
+
+// Animation
+const Duration kAnimationDuration = Duration(milliseconds: 250);
+const Duration kDefaultDuration = Duration(milliseconds: 250);
---
+++ lib/online_shop/home_screen/shop_home_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopHomeScreen extends StatelessWidget {
+  const ShopHomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+    );
+  }
+}
